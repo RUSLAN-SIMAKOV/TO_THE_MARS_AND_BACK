61136cf5a4d0db9e46b363b12bbe665f3ac548cc
RUSLAN SIMAKOV <ua667766706@gmail.com>
2021-06-16 22:28:44 +0300

    first commit

---
+++ src/main/java/ruslan/simakov/to_the_mars_and_back/ToTheMarsAndBackApplication.java
@@ -0,0 +1,13 @@
+package ruslan.simakov.to_the_mars_and_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToTheMarsAndBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToTheMarsAndBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ruslan/simakov/to_the_mars_and_back/ToTheMarsAndBackApplicationTests.java
@@ -0,0 +1,13 @@
+package ruslan.simakov.to_the_mars_and_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToTheMarsAndBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
